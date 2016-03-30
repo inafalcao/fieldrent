25f0f53963afdbaf2cc53d5a7712b0df96449529
inafalcao <inafalcao@gmail.com>
2016-03-29 21:58:58 -0300

    Preparing for production.

--- fieldrent/src/main/java/br/com/fieldrent/FieldrentApplication.java
+++ fieldrent/src/main/java/br/com/fieldrent/FieldrentApplication.java
@@ -2,11 +2,18 @@ package br.com.fieldrent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FieldrentApplication {
+public class FieldrentApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FieldrentApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(FieldrentApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(FieldrentApplication.class, args);
+    }
 }
