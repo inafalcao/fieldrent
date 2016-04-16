09fbdb10adcc3fcc16a0e768fb83ea70b633ca7a
inafalcao <inafalcao@gmail.com>
2016-04-16 18:23:37 -0300

    Reservation model adjustments.

--- fieldrent/src/main/java/br/com/fieldrent/model/Reservation.java
+++ fieldrent/src/main/java/br/com/fieldrent/model/Reservation.java
@@ -1,8 +1,13 @@
 package br.com.fieldrent.model;
 
+import br.com.fieldrent.ScheduleDeserializer;
+import br.com.fieldrent.ScheduleSerializer;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import org.hibernate.annotations.Type;
 import org.joda.time.DateTime;
+import org.joda.time.LocalTime;
 
 import javax.persistence.*;
 import javax.persistence.Entity;
@@ -26,13 +31,89 @@ public class Reservation extends br.com.fieldrent.model.Entity {
     private Field field;
 
     @NotNull
-    @Column(name = "start_time")
+    @Column(name = "date")
     @Type(type="org.jadira.usertype.dateandtime.joda.PersistentDateTime")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
     private DateTime date;
+
+    @NotNull
+    @Column(name = "start_time")
+    @Type(type="org.jadira.usertype.dateandtime.joda.PersistentLocalTime")
+    //@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm")
+    @JsonSerialize(using = ScheduleSerializer.class)
+    @JsonDeserialize(using = ScheduleDeserializer.class)
+    private LocalTime startTime;
+
+    @NotNull
+    @Column(name = "end_time")
+    @Type(type="org.jadira.usertype.dateandtime.joda.PersistentLocalTime")
+    //@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm")
+    @JsonSerialize(using = ScheduleSerializer.class)
+    @JsonDeserialize(using = ScheduleDeserializer.class)
+    private LocalTime endTime;
 
     @Column(name = "reservation_status")
     @Enumerated(EnumType.STRING)
     private ReservationStatus reservationStatus;
 
+    public Reservation() {
+
+    }
+
+    public Reservation(Client client, Field field, DateTime date, LocalTime startTime, LocalTime endTime, ReservationStatus reservationStatus) {
+        this.client = client;
+        this.field = field;
+        this.date = date;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.reservationStatus = reservationStatus;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public Field getField() {
+        return field;
+    }
+
+    public void setField(Field field) {
+        this.field = field;
+    }
+
+    public DateTime getDate() {
+        return date;
+    }
+
+    public void setDate(DateTime date) {
+        this.date = date;
+    }
+
+    public ReservationStatus getReservationStatus() {
+        return reservationStatus;
+    }
+
+    public void setReservationStatus(ReservationStatus reservationStatus) {
+        this.reservationStatus = reservationStatus;
+    }
 }
